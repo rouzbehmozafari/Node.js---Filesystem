02b789dd1ba3b7989f29622236d26fc7506c3692
“rouzbehmozafari” <rouzbeh.mozafari@yahoo.com>
2022-01-06 18:56:00 +0100

    2_1 update

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const fs = require('fs')
-
+const data = require("./data.json");
+// console.log(data)
 fs.readFile('data.json',(err,file)=>{
     if (err) throw err
     else {
-        fs.writeFile('textData.txt', file.toString(),(err)=>{if(err) throw err})
+        let arr = data.map(i => `${i.id}\n${i.title}\n${i.description} \n\n`)
+        let narr = arr.join('')
+        fs.writeFile('textData.txt', narr,(err)=>{if(err) throw err})
     }
 })
+
+
+
+
+// fs.writeFileSync('textData.js', arr.toString())
